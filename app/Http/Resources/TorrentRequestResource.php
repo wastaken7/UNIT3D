50a756154c0e479fb1f0a451929e968c51416878
AnabolicsAnonymous <github@unhittable.pw>
2025-08-22 18:25:07 -0500

    chore: larastan

--- app/Http/Resources/TorrentRequestResource.php
+++ app/Http/Resources/TorrentRequestResource.php
@@ -18,10 +18,16 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin \App\Models\TorrentRequest
+ */
 class TorrentRequestResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array<string, mixed>
      */
     public function toArray($request): array
     {
